7e1f2eed7be9e05202af95bd1b73aa931fd5225e
Honzasko <straka.jan5@seznam.cz>
2022-08-18 14:08:45 +0200

    memory.c - Triple Fault fix #1

--- kernel/src/memory.c
+++ kernel/src/memory.c
@@ -5,9 +5,9 @@
 extern uint64_t GetCR3();
 extern void SetCR3(uint64_t ptr);
 
-struct stivale2_struct_tag_memmap memory_map;
+struct stivale2_struct_tag_memmap *memory_map;
 
 void InitializeMemory(struct stivale2_struct_tag_memmap *memmap)
 {
-    memory_map = *memmap;
+    memory_map = memmap;
 }
